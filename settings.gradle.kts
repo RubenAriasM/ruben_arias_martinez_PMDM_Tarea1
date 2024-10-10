fc07bb1d17fb40d5e471f2fb62bdddef121f11d7
RubenAriasM <rubenarmar2005@gmail.com>
2024-10-10 11:55:13 +0200

    Creación del proyecto.

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ruben_arias_martinez_PMDM_Tarea1"
+include(":app")
+ 
